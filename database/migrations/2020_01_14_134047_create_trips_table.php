9706c2c9adb4f4c4710fb6ee6d0293efa813690e
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-01-14 10:44:46 -0300

    -created model, controller (resource) and migration for trip entity;

---
+++ database/migrations/2020_01_14_134047_create_trips_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('begin_date');
+            $table->date('end_date');
+            $table->unsignedBigInteger('creator_id');
+            $table->foreign('creator_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
