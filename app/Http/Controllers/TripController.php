e52c7922b91cf0f0f22e5ff13d1b6583f422153e
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-01-16 19:49:28 -0300

    - added auth check to tripscontroller;

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index');
+    }
+
     /**
      * Display a listing of the resource.
      *
