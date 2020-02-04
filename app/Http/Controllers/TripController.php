c5128cb52c5d697ea9786209554fe0efd397729f
brunoFpassaglia <bruno.passaglia2013@gmail.com>
2020-02-03 22:44:10 -0300

    - removed default avatar from users;

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -8,17 +8,17 @@ use Illuminate\Http\Request;
 
 class TripController extends Controller
 {
-
+    
     public function __construct()
     {
         $this->middleware('auth')->except('index');
     }
-
+    
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function index()
     {
         //
@@ -30,79 +30,92 @@ class TripController extends Controller
     {
         //
         $trips = auth()->user()->trips;
-        // dd($trips);
         return view('trips.index')->with('trips', $trips)->with('pagetitle', 'Personal trips');;
     }
-
-
-
+    
+    
+    
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function create()
     {
         //
         return view('trips.create');
     }
-
+    
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
     public function store(CreateTripRequest $request)
     {
         //
         $data = $request->validated();
         auth()->user()->ownsTrip()->create($data);
     }
-
+    
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Trip  $trip
-     * @return \Illuminate\Http\Response
-     */
+    * Display the specified resource.
+    *
+    * @param  \App\Trip  $trip
+    * @return \Illuminate\Http\Response
+    */
     public function show(Trip $trip)
     {
         //
         return view('trips.show')->with('trip', $trip);
     }
-
+    
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Trip  $trip
-     * @return \Illuminate\Http\Response
-     */
+    * Show the form for editing the specified resource.
+    *
+    * @param  \App\Trip  $trip
+    * @return \Illuminate\Http\Response
+    */
     public function edit(Trip $trip)
     {
         //
     }
-
+    
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Trip  $trip
-     * @return \Illuminate\Http\Response
-     */
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \App\Trip  $trip
+    * @return \Illuminate\Http\Response
+    */
     public function update(Request $request, Trip $trip)
     {
         //
     }
-
+    
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Trip  $trip
-     * @return \Illuminate\Http\Response
-     */
+    * Remove the specified resource from storage.
+    *
+    * @param  \App\Trip  $trip
+    * @return \Illuminate\Http\Response
+    */
     public function destroy(Trip $trip)
     {
         //
+    }
+    
+    
+    public function joinTrip(Trip $trip){
+        
+        auth()->user()->trips()->sync($trip);
+        return redirect()->route('trips.show', $trip);
+    }
+    
+    
+    public function quitTrip(Trip $trip){
+        
+        auth()->user()->trips()->detach($trip);
+        return redirect()->route('trips.show', $trip);
     }
 }
